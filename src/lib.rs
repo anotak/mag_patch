ce24db5a4b478d7a1df569f7a2078a81a44c47a7
anotak <anotak@users.noreply.github.com>
2025-05-11 00:19:21 +0200

    file left out of last commit

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@ impl std::fmt::Display for MpError {
 fn attach() -> Result<(), Box<dyn std::error::Error>> {
     crate::character_tick::hook_character_ticks()?;
     
-    make_hook(EXE_BASE + 0xFB7A0, execute_anmchr_command as usize)?;
+    ExecuteAnmChrCommandFn::make_hook(EXE_BASE + 0xFB7A0, execute_anmchr_command)?;
     
     
     debug_msg("mag_patch hooking success!\nthis is a hella alpha version 4 please dont share it");
@@ -68,12 +68,12 @@ fn attach() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+pub type ExecuteAnmChrCommandFn = unsafe extern "win64" fn(usize, usize);
 /// original is at EXE_BASE + 0xFB7A0,
 pub extern "win64" fn execute_anmchr_command(executor_ptr : usize, anmchr_command_ptr : usize)
 {
     use crate::game_data::Char;
     
-    let original : fn(usize, usize) -> () = get_original_func!(execute_anmchr_command);
     let command_type_group = unsafe { read_ptr::<u32>(anmchr_command_ptr as usize) };
     let command = unsafe { read_ptr::<u32>(anmchr_command_ptr + 4) };
     // i am not certain this is the correct way to do this
@@ -97,7 +97,9 @@ pub extern "win64" fn execute_anmchr_command(executor_ptr : usize, anmchr_comman
             }
         }
     }
-    original(executor_ptr, anmchr_command_ptr);
+    
+    let hook = ExecuteAnmChrCommandFn::get_original(execute_anmchr_command);
+    unsafe { hook.call(executor_ptr, anmchr_command_ptr) };
     
     reloads.restore();
 }
