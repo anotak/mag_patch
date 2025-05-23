b45b7571f939e38facebf1a0ed8277b89126af32
anotak <anotak@users.noreply.github.com>
2025-05-23 07:43:14 +0200

    doc comments for unary operators

--- src/unary_operators.rs
+++ src/unary_operators.rs
@@ -7,7 +7,7 @@ use crate::math::*;
 macro_rules! unary_operators {
     
     {
-        $( ( $id:literal, $name:ident,  $float_func:expr, $int_func:expr $(,)* ) ),+
+        $( ( $(#[$($attrss1:tt)*])* $id:literal, $(#[$($attrss2:tt)*])* $name:ident,  $float_func:expr, $int_func:expr $(,)* ) ),+
         $(,)*
     } => {
         use num_derive::FromPrimitive;
@@ -16,6 +16,8 @@ macro_rules! unary_operators {
         pub enum UnaryOp
         {
             $(
+                $(#[$($attrss1)*])*
+                $(#[$($attrss2)*])*
                 $name = $id,
             )+
         }
@@ -52,29 +54,38 @@ macro_rules! unary_operators {
 
 
 unary_operators! {
-    
     (
+        /// chop the fraction part off a number, so  4.0 or 4.1 or 4.5 or 4.9 will all become 4
+        /// (does nothing for integers)
         0x00, Floor,
         |value : f32| { value.floor() },
         |value : i32| { value }
     ),
     (
+        /// if there's a fraction part on a number, chop that off and get the next highest number so 5.0 or 4.1 or 4.5 or 4.9 will all become 5
+        /// (does nothing for integers)
         0x01, Ceil,
         |value : f32| { value.ceil() },
         |value : i32| { value }
     ),
     (
+        /// rounds to the nearest integer so 4.0 and 4.1 will become 4.0. and 4.5 or 4.9 will become 5
+        /// (does nothing for integers)
         0x02, Round,
         |value : f32| { value.round() },
         |value : i32| { value }
     ),
     (
+        /// keeps only the fraction part of a number. 0.1, 1.1, 1234.1 will all become 0.1
+        /// (returns 0 for integers)
         0x03, Fract,
         |value : f32| { value.fract() },
         |_value : i32| { 0 }
     ),
     
     (
+        /// takes the square root of the number, so 64 will give 8.
+        /// if the number is negative, then takes the absolute value and multiplies it by -1. so -64 will give -8
         0x10, SqrtWithNegative,
         |value : f32| {
             if value < 0.0 {
@@ -92,6 +103,8 @@ unary_operators! {
         }
     ),
     (
+        /// approximate sine of the number. approximation is used to prevent this from working differently on different cpus / compiler versions.
+        /// the approximation is here https://github.com/anotak/mag_patch/blob/main/src/math.rs#L58
         0x11, Sin,
         |value : f32| {
             approx_sin(value)
@@ -104,6 +117,8 @@ unary_operators! {
         }
     ),
     (
+        /// approximate cosine of the number. approximation is used to prevent this from working differently on different cpus / compiler versions.
+        /// the approximation is here https://github.com/anotak/mag_patch/blob/main/src/math.rs#L58
         0x12, Cos,
         |value : f32| {
             approx_cos(value)
@@ -118,23 +133,32 @@ unary_operators! {
     
     
     (
+        /// absolute value
         0x20, Abs,
         |value : f32| { value.abs() },
         |value : i32| { value.abs() }
     ),
     (
+        /// the sign of the number.
+        /// if it's negative, it gives -1.
+        /// positive gives 1.
+        /// 0 gives 0.
         0x21, Signum,
         |value : f32| { value.signum() },
         |value : i32| { value.signum() }
     ),
     
     (
+        /// 1 if the value is positive
+        /// 0 if 0 or negative
         0x30, IsPositive,
         |value : f32| { f32_bool(value > 0.0) },
         |value : i32| { i32_bool(value.is_positive()) }
     ),
     
     (
+        /// flips all the bits 0 to 1 and 1 to 0. see also https://en.wikipedia.org/wiki/Bitwise_operation#NOT
+        /// note that doing this on floating point numbers won't be very useful as mag_patch resets any NaN/infinity floats to 0.0
         0xB0, BitwiseNot,
         |value : f32| {
             f32::from_bits(!value.to_bits())
@@ -143,6 +167,8 @@ unary_operators! {
     ),
     
     (
+        /// if nonzero then returns 0.0
+        /// if 0 then returns 1.0
         0xC0, LogicalNot,
         |value : f32| {
             f32_bool(!is_f32_true(value))
